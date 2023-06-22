cd3b076a29e64392dd1f8cd2cfe84fec5c33133e
Eric Schmale <eric.schmale@gmx.de>
2023-06-22 20:12:47 +0200

    column names to table in UI

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use calamine::{open_workbook, Reader, Xlsx};
+use serde::{Deserialize, Serialize};
+
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -9,7 +12,24 @@ fn greet(name: &str) -> String {
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, get_excel_column_names])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+#[tauri::command]
+fn get_excel_column_names (path: String) -> String {
+    
+    let mut excel: Xlsx<_> = open_workbook(path).unwrap();
+    let mut names: Vec<String> = Vec::new();
+    if let Some(Ok(r)) = excel.worksheet_range("EIS-DTA") {
+        for row in r.rows() {
+            for cell in row.iter() {
+                names.push(cell.to_string());
+            }
+            break;
+        }
+    }
+    let json = serde_json::to_string(&names).unwrap();
+    json
 }
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { open } from '@tauri-apps/api/dialog';
 
 let greetInputEl: HTMLInputElement | null;
 let greetMsgEl: HTMLElement | null;
-
 async function greet() {
   if (greetMsgEl && greetInputEl) {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
@@ -32,4 +31,21 @@ btnExcelFile?.addEventListener("click", async () => {
     ]
   });
   console.log(selected);
+  let testdata: string = await invoke("get_excel_column_names", {path: selected});
+  
+  // testdata is ja json object put it in an object
+  let testdata2: string[] = JSON.parse(testdata);
+  let divExcelColumns = document.querySelector("#divExcelColumns");
+  for (let i = 0; i < testdata2.length; i++) {
+    // print i and testdata2[i] in a table
+    let tr = document.createElement("tr");
+    let td1 = document.createElement("td");
+    let td2 = document.createElement("td");
+    let tempcolumnnr = i + 1;
+    td1.textContent = tempcolumnnr.toString();
+    td2.textContent = testdata2[i];
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    divExcelColumns?.appendChild(tr);
+  }
 });
