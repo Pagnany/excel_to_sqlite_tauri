b0f033f5eb38698744854fd7b07c898a5f967200
Pagnany <eric.schmale@gmx.de>
2023-06-22 08:29:36 +0200

    first commit

---
+++ src/main.ts
@@ -0,0 +1,38 @@
+import { invoke } from "@tauri-apps/api/tauri";
+import { open } from '@tauri-apps/api/dialog';
+import { appDir } from '@tauri-apps/api/path';
+
+
+let greetInputEl: HTMLInputElement | null;
+let greetMsgEl: HTMLElement | null;
+
+async function greet() {
+  if (greetMsgEl && greetInputEl) {
+    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    greetMsgEl.textContent = await invoke("greet", {
+      name: greetInputEl.value,
+    });
+  }
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  greetInputEl = document.querySelector("#greet-input");
+  greetMsgEl = document.querySelector("#greet-msg");
+  document.querySelector("#greet-form")?.addEventListener("submit", (e) => {
+    e.preventDefault();
+    greet();
+  });
+});
+
+const selected = await open({
+  directory: true,
+  multiple: true,
+  defaultPath: await appDir(),
+});
+if (Array.isArray(selected)) {
+  // user selected multiple directories
+} else if (selected === null) {
+  // user cancelled the selection
+} else {
+  // user selected a single directory
+}
